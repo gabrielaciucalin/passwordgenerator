84daa6e78efe089776ea18b2855d1ded5225597e
gabrielaaciucalin <93832764+gabrielaaciucalin@users.noreply.github.com>
2024-05-22 18:38:28 +0300

    source code

---
+++ password.py
@@ -0,0 +1,13 @@
+import random
+
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = ",./;'()*^&$%###!@"
+
+all = lower+upper+numbers+symbols
+length=8
+
+password = "".join(random.sample(all,length))
+
+print(password)
